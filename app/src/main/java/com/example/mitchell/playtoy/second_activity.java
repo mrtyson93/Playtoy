66c9d9d94ca75cb6888b95207bb225177459b092
Mitchell Tyson <mrtyson@umich.edu>
2015-05-17 14:03:23 -0400

    got onclick listener for open screen, button goes to the second activity now, will work on second activity next lesson learned, each xml file will have a corresponding java file where that will have the executable stuff

--- app/src/main/java/com/example/mitchell/playtoy/second_activity.java
+++ app/src/main/java/com/example/mitchell/playtoy/second_activity.java
@@ -4,36 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class second_activity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //***This does main app shit**//
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //these make the buttons/textView = the ones from activity_main.xml
-        Button open_Btn = (Button) findViewById(R.id.contBtn);
-        final TextView testing = (TextView) findViewById(R.id.textView1);
-
-        //this sets the view to the second activity, should be done with this now
-        open_Btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_second_activity); //sets title to all caps
-            }
-        });
+        setContentView(R.layout.activity_second_activity);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_second_activity, menu);
         return true;
     }
 
